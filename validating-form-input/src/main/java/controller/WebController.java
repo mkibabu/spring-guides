668759eb517a381a94d6454bfac1fc9a59a27c35
Michael Muraya <mkibabu@gmail.com>
2015-10-22 11:50:01 -0400

    Tweak controller

--- validating-form-input/src/main/java/controller/WebController.java
+++ validating-form-input/src/main/java/controller/WebController.java
@@ -11,25 +11,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import domain.Person;
 
+
 @Controller
 public class WebController extends WebMvcConfigurerAdapter {
-    
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/results").setViewName("results");
     }
-    
-    /*
-     * Returns the form template.
-     */
+
     @RequestMapping(value="/", method=RequestMethod.GET)
     public String showForm(Person person) {
         return "form";
     }
-    
-    /*
-     * Binds form attributes to the Person object, then tests for and retrieves validation errors.
-     */
+
     @RequestMapping(value="/", method=RequestMethod.POST)
     public String checkPersonInfo(@Valid Person person, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
@@ -37,5 +32,5 @@ public class WebController extends WebMvcConfigurerAdapter {
         }
         return "redirect:/results";
     }
-    
+
 }
