ae4f4464752fa198c25aa2c91f93fca5de7fe4d5
Michael Muraya <mkibabu@gmail.com>
2015-10-29 10:07:59 -0400

    Modify package name in form validation project

--- 04-validating-form-input/src/main/java/validate/Application.java
+++ 04-validating-form-input/src/main/java/validate/Application.java
@@ -1,4 +1,4 @@
-package val;
+package validate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- 04-validating-form-input/src/main/java/validate/Person.java
+++ 04-validating-form-input/src/main/java/validate/Person.java
@@ -1,4 +1,4 @@
-package val;
+package validate;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
