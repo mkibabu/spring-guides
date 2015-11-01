07dde9818cb85a54c6a7b1f1e29dd44a4b5e926a
Michael Muraya <mkibabu@gmail.com>
2015-11-01 06:07:01 -0500

    Add message receiver

---
+++ 07-redis-messaging/src/main/java/receiver/Receiver.java
@@ -0,0 +1,25 @@
+package receiver;
+
+import java.util.concurrent.CountDownLatch;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class Receiver {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Receiver.class);
+	
+	private CountDownLatch latch;
+	
+	@Autowired
+	public Receiver(CountDownLatch latch) {
+		this.latch = latch;
+	}
+	
+	public void receiveMessage(String message) {
+		LOG.info("Received ," + message + ">");
+		latch.countDown();
+	}
+	
+}
